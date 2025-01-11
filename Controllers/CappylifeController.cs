eb0eb9b3fbb728c60a5b10b051f0db52a01719b7
Fa1thinyourself <136361625+Fa1thinyourself@users.noreply.github.com>
2025-01-11 08:51:35 +0700

    add cappylife

---
+++ Controllers/CappylifeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using CappypopMVC.Models.DatabaseModels;
+using Microsoft.AspNetCore.Mvc;
+using CappypopMVC.Models;
+using Supabase;
+
+namespace CappypopMVC.Controllers
+{
+    public class CappylifeController : Controller
+    {
+        private readonly ILogger<CappylifeController> _logger;
+        private readonly Client supabase;
+
+        public CappylifeController(ILogger<CappylifeController> logger, Client client)
+        {
+            _logger = logger;
+            supabase = client;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await supabase.From<Cappylife>().Get();
+            var cappylife = response.Models;
+            return View(cappylife);
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var cappylife = await supabase.From<Cappylife>().Where(x => x.Id == id).Single();
+
+            if (cappylife == null)
+            {
+                return NotFound();
+            }
+            return View(cappylife);
+        }
+    }
+}
---
+++ Models/DatabaseModels/Cappylife.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace CappypopMVC.Models.DatabaseModels;
+
+[Table("blog")]
+public class Cappylife : BaseModel
+{
+    [PrimaryKey("id")]
+    public int Id { get; set; }
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("createat")]
+    public DateTime? CreatedAt { get; set; }
+
+    [Column("category")]
+    public string? Category { get; set; }
+
+    [Column("photoURL")]
+    public string? PhotoURL { get; set; }
+
+    [Column("context")]
+    public string? Context { get; set; }
+    [Column("description")]
+    public string? Description { get; set; }
+}
